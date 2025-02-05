698d7e093ce8d01c42c774e949b15b8139a142b7
Lindar <lindarolostur@gmail.com>
2025-02-05 16:14:01 +0300

    999

--- Sources/UtilityHelper/UtilityHelper.swift
+++ Sources/UtilityHelper/UtilityHelper.swift
@@ -19,31 +19,7 @@ public struct ProductsBuy {
 //    @MainActor public static let shared = ProductsBuy()
 //    public init() {}
     
-    public var subscriptions: [AppProduct] = []
     
-    public enum Products: String {
-        case year, week, error
-    }
-
-    public struct AppProduct: Identifiable {
-        public let id = UUID()
-        public let item: ApphudProduct
-        public let type: Products
-        public let price: String
-        
-        public init(item: ApphudProduct) {
-            self.item = item
-            self.type = {
-                if item.productId.contains("year") || item.productId.contains("annual") {
-                    return .year
-                } else if item.productId.contains("week") {
-                    return .week
-                }
-                return .error
-            }()
-            self.price = item.skProduct?.localizedPrice ?? "$0"
-        }
-    }
 }
 
 public struct TextBuilder: View {
@@ -87,6 +63,32 @@ public struct UtilityHelper {
     public static func showMessage() {
         print("Hello from UtilityHelper!")
     }
-    public var ler = "Lol"
+    public static let ler = "Lol"
+    
+    @State var subscriptions: [AppProduct] = []
+    
+    public enum Products: String {
+        case year, week, error
+    }
+
+    public struct AppProduct: Identifiable {
+        public let id = UUID()
+        public let item: ApphudProduct
+        public let type: Products
+        public let price: String
+        
+        public init(item: ApphudProduct) {
+            self.item = item
+            self.type = {
+                if item.productId.contains("year") || item.productId.contains("annual") {
+                    return .year
+                } else if item.productId.contains("week") {
+                    return .week
+                }
+                return .error
+            }()
+            self.price = item.skProduct?.localizedPrice ?? "$0"
+        }
+    }
 }
 #endif
