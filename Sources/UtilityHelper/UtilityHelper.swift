54872c409a3b008c468fc2de2f416b0f1ca51d88
Lindar <lindarolostur@gmail.com>
2025-02-05 16:14:01 +0300

    Try to get all func

--- Sources/UtilityHelper/UtilityHelper.swift
+++ Sources/UtilityHelper/UtilityHelper.swift
@@ -6,26 +6,21 @@ import StoreKit
 import Foundation
 import SwiftUICore
 #if os(iOS)
-public extension SKProduct {
-    var localizedPrice: String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.locale = priceLocale
-        return formatter.string(from: price)!
-    }
-}
 
-public struct ProductsBuy {
-//    @MainActor public static let shared = ProductsBuy()
-//    public init() {}
-    
-    public var subscriptions: [AppProduct] = []
-    
-    public enum Products: String {
+public struct UtilityHelper {
+    public static func showMessage() {
+        print("Hello from UtilityHelper!")
+    }
+
+    public static let ler = "Lol" // Добавляем public
+
+    @State public var subscriptions: [AppProduct] = [] // Добавляем public
+
+    public enum Products: String { // Добавляем public
         case year, week, error
     }
 
-    public struct AppProduct: Identifiable {
+    public struct AppProduct: Identifiable { // Добавляем public
         public let id = UUID()
         public let item: ApphudProduct
         public let type: Products
@@ -45,48 +40,13 @@ public struct ProductsBuy {
         }
     }
 }
-
-public struct TextBuilder: View {
-    public let string: String
-    public var size: CGFloat = 16
-    public var fontName: String = ""
-    public var weight: Font.Weight = .regular
-    public var color: Color = .black
-    public var opacity: Double = 1.0
-    public var alignment: TextAlignment = .leading
-    
-    public var body: some View {
-        getAlign(
-            t: getColor(
-                t: getWeight(
-                    t: getFontNSize(
-                        t: string,
-                        s: size,
-                        f: fontName),
-                    w: weight),
-                c: color,
-                o: opacity),
-            a: alignment)
-    }
-    private func getFontNSize(t: String, s: CGFloat = 16, f: String) -> Text {
-        Text(t).font(.custom(f, size: s))
-    }
-    private func getWeight(t: Text, w: Font.Weight = .regular) -> Text {
-        t.fontWeight(w)
-    }
-    private func getColor(t: Text, c: Color, o: Double) -> Text {
-        t.foregroundColor(c.opacity(o))
-    }
-    private func getAlign(t: Text, a: TextAlignment) -> some View {
-        t.multilineTextAlignment(a)
-    }
-}
-
-
-public struct UtilityHelper {
-    public static func showMessage() {
-        print("Hello from UtilityHelper!")
-    }
-    public var ler = "Lol"
-}
 #endif
+
+public extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price)!
+    }
+}
