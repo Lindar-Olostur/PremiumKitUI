bbefc564f0c99cc788d4ac9e2fe6275659e2ea56
Lindar <lindarolostur@gmail.com>
2025-02-05 15:01:17 +0300

    fix

--- Sources/UtilityHelper/ProductsBuy.swift
+++ Sources/UtilityHelper/ProductsBuy.swift
@@ -6,47 +6,47 @@
 //
 
 
-import ApphudSDK
-import StoreKit
-import Foundation
-import SwiftUICore
-
-extension SKProduct {
-    var localizedPrice: String {
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currency
-        formatter.locale = priceLocale
-        return formatter.string(from: price)!
-    }
-}
-
-public class ProductsBuy {
-    @MainActor public static let shared = ProductsBuy()
-    private init() {}
-    
-    public var subscriptions: [AppProduct] = []
-    
-    public enum Products: String {
-        case year, week, error
-    }
-
-    public struct AppProduct: Identifiable {
-        public let id = UUID()
-        public let item: ApphudProduct
-        public let type: Products
-        public let price: String
-        
-        public init(item: ApphudProduct) {
-            self.item = item
-            self.type = {
-                if item.productId.contains("year") || item.productId.contains("annual") {
-                    return .year
-                } else if item.productId.contains("week") {
-                    return .week
-                }
-                return .error
-            }()
-            self.price = item.skProduct?.localizedPrice ?? "$0"
-        }
-    }
-}
+//import ApphudSDK
+//import StoreKit
+//import Foundation
+//import SwiftUICore
+//
+//extension SKProduct {
+//    var localizedPrice: String {
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .currency
+//        formatter.locale = priceLocale
+//        return formatter.string(from: price)!
+//    }
+//}
+//
+//public class ProductsBuy {
+//    @MainActor public static let shared = ProductsBuy()
+//    private init() {}
+//    
+//    public var subscriptions: [AppProduct] = []
+//    
+//    public enum Products: String {
+//        case year, week, error
+//    }
+//
+//    public struct AppProduct: Identifiable {
+//        public let id = UUID()
+//        public let item: ApphudProduct
+//        public let type: Products
+//        public let price: String
+//        
+//        public init(item: ApphudProduct) {
+//            self.item = item
+//            self.type = {
+//                if item.productId.contains("year") || item.productId.contains("annual") {
+//                    return .year
+//                } else if item.productId.contains("week") {
+//                    return .week
+//                }
+//                return .error
+//            }()
+//            self.price = item.skProduct?.localizedPrice ?? "$0"
+//        }
+//    }
+//}
--- Sources/UtilityHelper/UtilityHelper.swift
+++ Sources/UtilityHelper/UtilityHelper.swift
@@ -1,7 +1,50 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import SwiftUI
+import ApphudSDK
+import StoreKit
+import Foundation
+import SwiftUICore
+
+extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price)!
+    }
+}
+
+public class ProductsBuy {
+    @MainActor public static let shared = ProductsBuy()
+    private init() {}
+    
+    public var subscriptions: [AppProduct] = []
+    
+    public enum Products: String {
+        case year, week, error
+    }
+
+    public struct AppProduct: Identifiable {
+        public let id = UUID()
+        public let item: ApphudProduct
+        public let type: Products
+        public let price: String
+        
+        public init(item: ApphudProduct) {
+            self.item = item
+            self.type = {
+                if item.productId.contains("year") || item.productId.contains("annual") {
+                    return .year
+                } else if item.productId.contains("week") {
+                    return .week
+                }
+                return .error
+            }()
+            self.price = item.skProduct?.localizedPrice ?? "$0"
+        }
+    }
+}
 
 public struct TextBuilder: View {
     public let string: String
