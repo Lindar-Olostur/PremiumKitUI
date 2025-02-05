4227e961973c039b784c47214757470df3dd8d92
Lindar <lindarolostur@gmail.com>
2025-02-05 14:47:32 +0300

    add apphud subs

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "UtilityHelper",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UtilityHelper",
             targets: ["UtilityHelper"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apphud/ApphudSDK", "3.5.9"..<"4.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "UtilityHelper"),
-
+            name: "UtilityHelper",
+            dependencies: [
+                .product(name: "ApphudSDK", package: "ApphudSDK") // Link ApphudSDK
+            ]),
     ]
 )
--- Sources/UtilityHelper/UtilityHelper.swift
+++ Sources/UtilityHelper/UtilityHelper.swift
@@ -1,2 +1,40 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+public struct TextBuilder: View {
+    public let string: String
+    public var size: CGFloat = 16
+    public var fontName: String = ""
+    public var weight: Font.Weight = .regular
+    public var color: Color = .black
+    public var opacity: Double = 1.0
+    public var alignment: TextAlignment = .leading
+    
+    public var body: some View {
+        getAlign(
+            t: getColor(
+                t: getWeight(
+                    t: getFontNSize(
+                        t: string,
+                        s: size,
+                        f: fontName),
+                    w: weight),
+                c: color,
+                o: opacity),
+            a: alignment)
+    }
+    private func getFontNSize(t: String, s: CGFloat = 16, f: String) -> Text {
+        Text(t).font(.custom(f, size: s))
+    }
+    private func getWeight(t: Text, w: Font.Weight = .regular) -> Text {
+        t.fontWeight(w)
+    }
+    private func getColor(t: Text, c: Color, o: Double) -> Text {
+        t.foregroundColor(c.opacity(o))
+    }
+    private func getAlign(t: Text, a: TextAlignment) -> some View {
+        t.multilineTextAlignment(a)
+    }
+}
